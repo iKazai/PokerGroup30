f3c583fcc22144c766f9dcc8cceedab0171cb53f
iKazai <91919794+iKazai@users.noreply.github.com>
2025-03-18 10:51:25 +0100

    Correction des probabilités dans C2.md et ajout de l'exportation des approximations dans C4.c, avec histogrammes dans C4.r

---
+++ theoretical_study/C4.c
@@ -0,0 +1,18 @@
+#include <C3.c>
+
+int export_approx_1(int n){
+    double* approx = P_approx_1_n(n);
+    FILE *file = fopen("data.csv", "w");
+    if (file == NULL) {
+        perror("[export_approx_1] : Fichier invalide");
+        return -1;
+    }
+
+    for (int i = 0; i < 9; i++) {
+        fprintf(file, "%f\n", approx[i]);
+    }
+
+    fclose(file);
+    return 0;
+}
+

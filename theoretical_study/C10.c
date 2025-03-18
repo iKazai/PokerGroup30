c90c8663fdb508f3c20637289068af892a87edb5
iKazai <91919794+iKazai@users.noreply.github.com>
2025-03-18 10:27:34 +0100

    Tache C10 probabilité alternative

--- theoretical_study/C10.c
+++ theoretical_study/C10.c
@@ -1,5 +1,27 @@
 #include "TacheC3.h"
+#include <stdlib.h>
 
 double P_approx_2_1_alter(int i, int j, int n){
-    
+    double res = 0;
+    for (int k = 1; k < 4; k++)
+    {
+        for (int l = 1; l < 4; l++)
+        {
+            res += P_approx_1(k,l,n) * P_approx_2_1(k,l,i,j,n);
+        }
+    }
+    return res;
+}
+
+double* P_approx_2_alter(int n){
+    double* res = (double*)malloc(double * 9);
+    int index = 0;
+    for (int k = 1; k < 4; k++)
+    {
+        for (int l = 1; l < 4; l++)
+        {
+            res[index++] = P_approx_2_1(k,l,n);
+        }
+    }
+    return res;
 }

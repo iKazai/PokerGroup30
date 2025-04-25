10f5aba3c41733b82bc75d5e5dbefbce8592f274
Anjy STADELMANN <anjy.stadelmann@pc228-07.pedago.ensiie.fr>
2025-04-25 14:42:19 +0200

    Correction de miroir.c : find max et min et agressive

--- research_30/experiments/miroir.h
+++ research_30/experiments/miroir.h
@@ -5,7 +5,7 @@
 #include "player.h"
 #include <stdbool.h>
 
-/*
+/** 
 * \brief Génère un pari aléatoire pour un joueur
 * \param Un joueur
 * \return aléatoire un paris : true (Victoire) ou false (Défaite)
@@ -14,21 +14,21 @@ void paris_aleatoire(player p);
 
 
 
-/*
+/** 
 * \brief Renvoie la carte avec la valeur minimale dans la main d'un joueur
 * \param Un joueur
 * \return Une carte correspondant à la carte avec valeur minimale dans la main du joueur
 */
-card find_min_card_value(player p);
+card find_min_card(player p);
 
-/*
+/** 
 * \brief Renvoie la carte avec la valeur maximal dans la main d'un joueur
 * \param Un joueur
 * \return Une carte correspondant à la carte avec valeur maximale dans la main du joueur
 */
-card find_max_card_value(player p);
+card find_max_card(player p);
 
-/*
+/**
 * \brief Implémente le style de jeu agressif
 * \param Un joueur
 * \return Un booléen qui renvoie vrai si la condition d'agressivité est remplie, faux sinon

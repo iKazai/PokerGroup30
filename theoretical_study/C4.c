a4ea68249495e3fca572af7be8a1b57412537b69
Criser05 <crisdemanou@gmail.com>
2025-04-25 15:04:14 +0200

    tache C4 commit

--- theoretical_study/C4.c
+++ theoretical_study/C4.c
@@ -1,4 +1,4 @@
-#include <C3.c>
+#include <C:\Users\crisd\OneDrive\PokerGroup30\theoretical_study\C3.c>
 
 int export_approx_1(int n){
     double* approx = P_approx_1_n(n);
@@ -8,7 +8,7 @@ int export_approx_1(int n){
         return -1;
     }
 
-    for (int i = 0; i < 9; i++) {
+    for (int i = 0; i < 6; i++) {
         fprintf(file, "%f\n", approx[i]);
     }
 

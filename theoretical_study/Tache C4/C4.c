348d68e41ee108e4358347a23a18ea91a6447035
Criser05 <crisdemanou@gmail.com>
2025-04-28 01:03:27 +0200

    Tache C.12

--- theoretical_study/Tache C4/C4.c
+++ theoretical_study/Tache C4/C4.c
@@ -1,10 +1,9 @@
 #include "C3.c"
-<<<<<<< HEAD
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdlib.h>
-=======
->>>>>>> 9d8c42cb9ae32f4f7a7e71895b927af4ba340913
+
+int export_approx_1(int n);
+
 
 int export_approx_1(int n) {
     double* approx = P_approx_1_n(n); 
@@ -19,17 +18,17 @@ int export_approx_1(int n) {
     fprintf(file, "Main;Probabilite\n");
 
 
-    double P_theo[6] = {1.0/3, 2.0/3, 2.0/3, 2.0/3, 4.0/15, 1.0/15};
     // Tableau des mains possibles (pour les labels)
-    const char* mains[6] = {"1-1", "1-2", "1-3","2-1", "2-2", "2-3", "3-3"};
+    const char* mains[7] = {"1-1", "1-2", "1-3","2-1", "2-2", "2-3", "3-3"};
 
     // Boucle pour écrire les données
     for (int i = 0; i < 7; i++) {
         fprintf(file, "%s;%f\n", mains[i], approx[i]);
     }
 
-    for (int n = 1; n <= 100000; n += 1000) {
-        double P_emp = P_approx_1_main(1, 2, n);
+
+    for (int n = 0; n <100000; n++){
+        double P_emp = P_approx_1(1, 2, n);
         fprintf(file, ",,,%d,%f\n", n, P_emp);
     }
 
@@ -40,12 +39,7 @@ int export_approx_1(int n) {
 
 
 int main() {
-    int result = export_approx_1(100000);  // Exemple avec n = 100000
-    if (result == 0) {
-        printf("Fichier CSV généré avec succès !\n");
-    } else {
-        printf("Échec de la génération du CSV.\n");
-    }
-    return 0;
+    export_approx_1(100000);
+    return 0;  
 }
 
